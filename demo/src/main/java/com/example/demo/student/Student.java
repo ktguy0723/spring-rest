a604e763c2b06d30da62b7d80da6e06b88478dff
ktguy0723 <ktguy.yy@gmail.com>
2021-09-04 17:07:28 +0900

    add transient

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table
@@ -26,21 +28,20 @@ public class Student {
   private String name;
   private String email;
   private LocalDate dob;
+  @Transient // DBにはなくプログラムで計算する
   private Integer age;
 
-  public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+  public Student(Long id, String name, String email, LocalDate dob) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.dob = dob;
-    this.age = age;
   }
 
-  public Student(String name, String email, LocalDate dob, Integer age) {
+  public Student(String name, String email, LocalDate dob) {
     this.name = name;
     this.email = email;
     this.dob = dob;
-    this.age = age;
   }
 
   // なぜかEntityにはデフォルトコンストラクタが必要
@@ -79,7 +80,7 @@ public class Student {
   }
 
   public Integer getAge() {
-    return age;
+    return Period.between(this.dob, LocalDate.now()).getYears();
   }
 
   public void setAge(Integer age) {
--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -17,15 +17,13 @@ public class StudentConfig {
       Student marian = new Student(
         "Marian",
         "marian.jamal@gmail.com",
-        LocalDate.of(2000, Month.JANUARY, 5),
-        21
+        LocalDate.of(2000, Month.JANUARY, 5)
       );
 
       Student alex = new Student(
         "Alex",
         "alex@gmail.com",
-        LocalDate.of(2010, Month.JULY, 22),
-        11
+        LocalDate.of(2010, Month.JULY, 22)
       );
 
       repository.saveAll(
