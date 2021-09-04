42f69cac74becd364cb7e7079449a032639f69a7
ktguy0723 <ktguy.yy@gmail.com>
2021-09-04 13:58:39 +0900

    add save mode

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -2,14 +2,33 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+  @Id
+  @SequenceGenerator(
+    name = "student_sequence",
+    sequenceName = "student_sequence",
+    allocationSize = 1
+  )
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "student_sequence"
+  )
   private Long id;
   private String name;
   private String email;
   private LocalDate dob;
   private Integer age;
 
-  public Student( Long id, String name, String email, LocalDate dob, Integer age) {
+  public Student(Long id, String name, String email, LocalDate dob, Integer age) {
     this.id = id;
     this.name = name;
     this.email = email;
@@ -23,6 +42,9 @@ public class Student {
     this.dob = dob;
     this.age = age;
   }
+
+  // なぜかEntityにはデフォルトコンストラクタが必要
+  public Student() {}
 
   public Long getId() {
     return id;
---
+++ readme.md
@@ -0,0 +1,4 @@
+## 参考サイト
+https://terasolunaorg.github.io/guideline/public_review/Introduction/Introduction.html
+
+https://youtu.be/9SGDpanrc8U
